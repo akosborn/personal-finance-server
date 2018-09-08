564525e9684e08c288d5d90756973e08f180fdb3
akosborn <andrewosborn93@gmail.com>
2018-09-08 18:38:16 -0400

    Created User model and updated MyUserDetails constructor

---
+++ src/main/java/com/osbornandrew/personal/finance/server/users/MyUserDetails.java
@@ -0,0 +1,54 @@
+package com.osbornandrew.personal.finance.server.users;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyUserDetails implements UserDetails {
+
+    /**
+     * Returns new MyUserDetails object with User param
+     * @param user User
+     */
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
--- src/main/java/com/osbornandrew/personal/finance/server/users/User.java
+++ src/main/java/com/osbornandrew/personal/finance/server/users/User.java
@@ -1,44 +1,31 @@
 package com.osbornandrew.personal.finance.server.users;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.Collection;
+public class User {
 
-public class User implements UserDetails {
+    public User() {}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
+    @Getter
+    private Long id;
+    /**
+     * Ex: "Andrew Osborn"
+     * Google: name
+     * Twitter: name
+     * */
+    @Getter @Setter
+    private String displayName;
+    /**
+     * Google: email
+     * Twitter: null -- API doesn't provide email
+     */
+    @Getter @Setter
+    private String email;
+    /**
+     * Google: id
+     * Twitter: id_str
+     * */
+    @Getter @Setter
+    private String socialId;
 }
