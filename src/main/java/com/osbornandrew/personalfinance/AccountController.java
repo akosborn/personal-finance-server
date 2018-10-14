0de9e46e66a93f580ccf33bc3a25da4da9e415de
akosborn <andrewosborn93@gmail.com>
2018-10-14 00:49:39 -0400

    Added json annotations for polymorphic deserialization

--- src/main/java/com/osbornandrew/personalfinance/AccountController.java
+++ src/main/java/com/osbornandrew/personalfinance/AccountController.java
@@ -1,8 +1,8 @@
 package com.osbornandrew.personalfinance;
 
+import com.osbornandrew.personalfinance.accounts.Account;
 import com.osbornandrew.personalfinance.accounts.CheckingAccount;
 import com.osbornandrew.personalfinance.users.MyUserDetails;
-import com.osbornandrew.personalfinance.users.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,33 +17,30 @@ import java.util.Set;
 
 @Secured("ROLE_USER")
 @RestController
-@RequestMapping("/api/checking")
-public class CheckingController {
+@RequestMapping("/api")
+public class AccountController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private WalletService walletService;
-    private CheckingService checkingService;
     private AccountService acctService;
 
     @Autowired
-    public CheckingController(WalletService walletService, CheckingService checkingService,
-                              AccountService acctService) {
+    public AccountController(WalletService walletService,
+                             AccountService acctService) {
         this.walletService = walletService;
-        this.checkingService = checkingService;
         this.acctService = acctService;
     }
 
-    @PostMapping("")
-    public Set<CheckingAccount> postAccount(@RequestBody CheckingAccount account) {
+    @PostMapping("/accounts")
+    public Account postAccount(@RequestBody Account account) {
 
-        Long userId = ((MyUserDetails) SecurityContextHolder.getContext()
-                .getAuthentication().getPrincipal()).getUser().getId();
+        Long userId = ((MyUserDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal()).getUser().getId();
         account.setWallet(walletService.loadByUserId(userId));
-        CheckingAccount savedAcct = (CheckingAccount) acctService.save(account);
-        log.info("Saved user (ID {}) checking account (ID {}) '{}' ", userId,
-                savedAcct.getId(), savedAcct.getName());
+        Account savedAcct = acctService.save(account);
+        log.info("Saved user {) account {}: '{}' ", userId, savedAcct.getId(), savedAcct.getName());
 
-        return checkingService.loadByUserId(userId);
+        return savedAcct;
     }
 }
