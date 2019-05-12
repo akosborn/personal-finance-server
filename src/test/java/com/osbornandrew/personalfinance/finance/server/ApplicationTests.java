9ed9ceb67faf6a7512692c754687f4e6d79343e2
akosborn <andrewosborn93@gmail.com>
2019-05-12 14:21:54 -0400

    Cleaned up tests

--- src/test/java/com/osbornandrew/personalfinance/finance/server/ApplicationTests.java
+++ src/test/java/com/osbornandrew/personalfinance/finance/server/ApplicationTests.java
@@ -4,8 +4,6 @@ import com.osbornandrew.personalfinance.accounts.CreditCard;
 import com.osbornandrew.personalfinance.accounts.Debt;
 import com.osbornandrew.personalfinance.accounts.Loan;
 import com.osbornandrew.personalfinance.repayment.FinanceUtils;
-import com.osbornandrew.personalfinance.repayment.PaymentPlan;
-import com.osbornandrew.personalfinance.repayment.RepaymentCalculator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,17 +34,5 @@ public class ApplicationTests {
 	public void payoffAmount() {
 		final DecimalFormat df = new DecimalFormat("#.##");
 		assert Double.valueOf(df.format(FinanceUtils.payoffAmount(18000, 0.011875, 24, 617.39))).equals(6866.97);
-	}
-
-	@Test
-	public void avalanchePan() {
-		Loan personal = new Loan("Discover", "personal", 6000, 184, 11f, 1);
-        CreditCard cc = new CreditCard("Citi", "meh", 900, 5000, 25, 19.5f, 1);
-        CreditCard b = new CreditCard("Chase", "meh", 40, 5000, 25, 19.5f, 1);
-        List<Debt> accounts = new ArrayList<>();
-        accounts.add(personal);
-        accounts.add(cc);
-        accounts.add(b);
-        RepaymentCalculator.buildAvalanchePlan(accounts, 300);
 	}
 }
